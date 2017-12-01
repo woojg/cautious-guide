c64032f8d564e74d5a62c2b3ed9cbb44de6ccc31
woo_jeonggeun <yakumo4@gmail.com>
2017-12-01 11:42:31 +0900

    feat: init

---
+++ cautious/cautious/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cautious
+//
+//  Created by 우정근 on 2017. 12. 1..
+//  Copyright © 2017년 woojg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
