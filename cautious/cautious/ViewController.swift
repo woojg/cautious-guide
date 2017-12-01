3afafc68de5abbf6c7d91b471f08d0c4a47fb0be
woo_jeonggeun <yakumo4@gmail.com>
2017-12-01 14:55:01 +0900

    add pods

--- cautious/cautious/ViewController.swift
+++ cautious/cautious/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Toaster
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Toast(text: "Hello, world!").show()
     }
 
     override func didReceiveMemoryWarning() {
